9fd736911f41fd1a158ca3dab6f42c75adc4c7c1
Ammar Asif <ammarasif509@gmail.com>
2026-08-22 20:06:56 +0500

    Fix Chrome deps hook - remove unavailable chromium package

--- .platform/hooks/predeploy/01_install_chrome_deps.sh
+++ .platform/hooks/predeploy/01_install_chrome_deps.sh
@@ -2,4 +2,4 @@
 sudo dnf install -y \
   nss atk cups-libs libdrm libxkbcommon at-spi2-atk \
   libXcomposite libXdamage libXrandr mesa-libgbm \
-  alsa-lib pango cairo chromium
+  alsa-lib pango cairo
